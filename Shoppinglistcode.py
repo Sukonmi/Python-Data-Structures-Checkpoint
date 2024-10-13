d748292a34b73a5c5de728c23491a08a56ea3049
Sukonmi <danielayomideh@gmail.com>
2024-10-13 16:53:40 +0100

    I edited the code

--- Shoppinglistcode.py
+++ Shoppinglistcode.py
@@ -4,17 +4,27 @@ shopping_list = []
 shopping_set = set()
 shopping_dict = {}
 
-#2. Use a while loop to create a menu of options for the user to add, remove, or view items from the list.
+# 2. Use a while loop to create a menu of options for the user to add, remove, or view items from the list.
 
 while True:
-# Display menu
-   print("\nMenu:\n1. Add item.\n2. Remove item.\n3. View items.\n4. Exit")
-   break
+    print("\nMENU: \n1. Add item. \n2. Remove item. \n3. View list. \n4. Exit.")
+    break
 
-#3. Use the input() function to prompt the user to make a selection from the menu.
-# Get user selection
-selection = input("Enter your choice (1-4): ")
+# 3. Use the input() function to prompt the user to make a selection from the menu.
 
-#  Set maximum number of items
-max_items = 5
+user_selection = input("Enter your choice (1-4): \n")
+
+# 4. Use an if-elif-else block to determine the user's selection and perform the corresponding action
+# a. If the user selects 'add', use the input() function to prompt the user to enter an item to add to the list. Use the range() function to limit the number of items that can be added to the list
+
+max_items = 10
+
+if user_selection == "1":
+    for i in range(max_items):
+        if len(shopping_list) < max_items:
+            item = input("Enter item to add (or 'stop' to finish): ")
+            if item.lower() == 'stop':
+                break
+
+
 
